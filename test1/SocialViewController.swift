7f29d76c51505ea9097cac240d19e150cecb6c6b
rainwolf <rainwolf@submanifold.be>
2017-02-13 22:32:41 +0100

    Social leader board

--- test1/SocialViewController.swift
+++ test1/SocialViewController.swift
@@ -15,7 +15,7 @@ import UIKit
 //    }
 //}
 
-class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, GADBannerViewDelegate, GADInterstitialDelegate {
+class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, GADBannerViewDelegate, GADInterstitialDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let segmentControl = UISegmentedControl(items: [NSLocalizedString("followers", comment: ""), NSLocalizedString("following", comment: "")])
     var refreshControl = UIRefreshControl()
@@ -29,7 +29,20 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var followers = [LivePlayer]()
     var following = [LivePlayer]()
     
+    let gameNames = ["Pente": 1, "Keryo-Pente": 3, "Gomoku": 5, "D-Pente": 7, "G-Pente": 9, "Poof-Pente": 11, "Connect6": 13,
+                     "Boat-Pente": 15, "Speed Pente": 2, "Speed Keryo-Pente": 4, "Speed Gomoku": 6, "Speed D-Pente": 8,
+                     "Speed G-Pente": 10, "Speed Poof-Pente": 12, "Speed Connect6": 14, "Speed Boat-Pente": 16,
+                     "Turn-based Pente": 51, "Turn-based Keryo-Pente": 53, "Turn-based Gomoku": 55, "Turn-based D-Pente": 57,
+                     "Turn-based G-Pente": 59, "Turn-based Poof-Pente": 61, "Turn-based Connect6": 63, "Turn-based Boat-Pente": 65]
+    let gameNamesArray = ["Turn-based Pente", "Turn-based Keryo-Pente", "Turn-based Gomoku", "Turn-based D-Pente",
+                          "Turn-based G-Pente", "Turn-based Poof-Pente", "Turn-based Connect6", "Turn-based Boat-Pente",
+                          "Pente", "Keryo-Pente", "Gomoku", "D-Pente", "G-Pente", "Poof-Pente", "Connect6",
+                          "Boat-Pente", "Speed Pente", "Speed Keryo-Pente", "Speed Gomoku", "Speed D-Pente",
+                          "Speed G-Pente", "Speed Poof-Pente", "Speed Connect6", "Speed Boat-Pente"]
+    let textField = UITextField()
+
     
+    var gameString: String?
     
     init(player: PentePlayer) {
         self.pentePlayer = player
@@ -46,7 +59,10 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.refreshControl.addTarget(self, action: #selector(refresh), for: UIControlEvents.valueChanged)
         self.tableView.addSubview(refreshControl)
         
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(addFollowing))
+        let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(addFollowing))
+        let settingsItem = UIBarButtonItem(image: UIImage(named: "gamesettings"), style: .plain, target: self, action: #selector(selectGame))
+        self.navigationItem.rightBarButtonItems = [addButton, settingsItem]
+        
     }
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
@@ -57,6 +73,10 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         tableView.frame = self.view.frame
         // Do any additional setup after loading the view.
+        gameString = UserDefaults.standard.string(forKey: "socialGame")
+        if gameString == nil {
+            gameString = "Pente"
+        }
         loadFollowersing()
     }
     
@@ -83,6 +103,8 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
             bannerView!.load(request)
             self.view.addSubview(bannerView!)
         }
+        
+        self.view.addSubview(textField)
     }
     override func viewDidDisappear(_ animated: Bool) {
         NotificationCenter.default.removeObserver(self)
@@ -120,6 +142,8 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
 //        cell.detailTextLabel?.attributedText = player.getRatingString(game: 1)
         cell.selectionStyle = .none
         cell.accessoryType = .disclosureIndicator
+        let game = gameNames[gameString!]!
+        cell.detailTextLabel?.attributedText = player.getRatingString(game: game)
         if wantsToSeeAvatars {
             if let image = pentePlayer.avatars.object(forKey: player.name) as? UIImage {
                 cell.imageView?.image = image
@@ -215,19 +239,21 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func loadFollowersing() {
         do {
+            let game = gameNames[gameString!]!
             followers.removeAll()
             following.removeAll()
-            let followersing = try String(contentsOf: URL(string: "https://www.pente.org/gameServer/mobile/followers.jsp")!, encoding: String.Encoding.utf8)
+            let followersing = try String(contentsOf: URL(string: "https://www.pente.org/gameServer/mobile/followers.jsp?game=\(game)")!, encoding: String.Encoding.utf8)
             //            let activeServers = try String(contentsOf: URL(string: "https://development.pente.org/gameServer/activeServers")!, encoding: String.Encoding.utf8)
 //                        print(followersing)
             let followerLines = followersing.components(separatedBy: "\n")
             for line in followerLines {
                 let splitLine: [String] = line.components(separatedBy: ";")
-                if splitLine.count > 4 {
+                if splitLine.count > 5 {
                     let player = LivePlayer(name: splitLine[1])
                     player.subscriber = (splitLine[2] == "1")
                     player.crown = Int(splitLine[4])!
                     player.color = UIColorFromRGB(rgbValue: Int(splitLine[3])!)
+                    player.ratings = [game: Int(splitLine[5])!]
                     if splitLine[0] == "1" {
                         following.append(player)
                     } else {
@@ -238,6 +264,12 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
                     }
                 }
             }
+            followers.sort { (p1, p2) -> Bool in
+                return p1.ratings[game]! > p2.ratings[game]!
+            }
+            following.sort { (p1, p2) -> Bool in
+                return p1.ratings[game]! > p2.ratings[game]!
+            }
         } catch let error {
             self.showErrorAlert(alertMessage: NSLocalizedString("There was an error loading followers. Reason: \(error.localizedDescription)", comment: ""))
         }
@@ -247,6 +279,47 @@ class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
+    func selectGame() {
+        let gamePicker = UIPickerView()
+        let pickerToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 44))
+        pickerToolbar.barStyle = .blackTranslucent
+        let extraSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target:nil, action:nil)
+        let doneButton = UIBarButtonItem(title: NSLocalizedString("Done", comment: ""), style: .done, target: self, action: #selector(dismissPicker)) // method
+        pickerToolbar.setItems([extraSpace, doneButton], animated: true)
+        gamePicker.delegate = self
+        gamePicker.dataSource = self
+        gamePicker.tag = 1
+//        var game = table.game
+//        if game%2 == 0 {
+//            game = game - 1
+//        }
+        gamePicker.selectRow(gameNamesArray.index(of: gameString!)!, inComponent: 0, animated: true)
+        
+        textField.inputView = gamePicker
+        textField.tag = 1;
+//        textField.delegate = self
+        textField.inputAccessoryView = pickerToolbar
+        
+        textField.becomeFirstResponder()
+    }
+    func dismissPicker() {
+        textField.resignFirstResponder()
+        loadFollowersing()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.gameNamesArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.gameNamesArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        gameString = self.gameNamesArray[row]
+        UserDefaults.standard.set(gameString, forKey: "socialGame")
+    }
 
 }
 
